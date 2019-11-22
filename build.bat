e102666ac9ee2ba2df58e4c740bf6a5a60ea7d41
Bilge Theall <wazcd_1608@qq.com>
2019-11-22 20:56:48 +0800

    add README.md modify build.bat

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ call build_client.bat
 call build_server.bat
 
 cd target
-copy Client-0.jar ../client.jar
-copy Server-0.jar ../server.jar
+copy Client-0.jar ..\client.jar
+copy Server-0.jar ..\server.jar
